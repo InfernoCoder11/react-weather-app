a419d08f1af092c86f0074d7623b6c999f1e68d5
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2020-03-28 16:08:59 +0530

    Rewritten for better UI

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -1,65 +1,50 @@
 import React from "react";
 import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
-import Collapse from "@material-ui/core/Collapse";
-import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
 import moment from "moment";
 
 class CityCard extends React.Component {
-  //[expanded, setExpanded] = React.useState(false);
   currently = this.props.weatherData[`${this.props.index}`]["currently"];
-  expanded = false;
-  handleExpandClick = () => {
-    console.log("Expand Click");
-    //setExpanded(!expanded);
-  };
+  daily = this.props.weatherData[`${this.props.index}`]["daily"];
   render() {
     return (
-      <Card>
-        <CardHeader
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title={this.props.index}
-          subheader={moment().format("dddd, MMMM Do YYYY h:mm a")}
-        />
-        <CardMedia image="" title="" />
-        <CardContent>
-          <Typography variant="body2" color="textPrimary" component="p">
-            {this.currently["summary"]}
-          </Typography>
-          <Typography variant="body2" color="textPrimary" component="p">
-            Temperature : {this.currently["temperature"]} C
-          </Typography>
-          <Typography variant="body2" color="textPrimary" component="p">
-            Humidity : {this.currently["humidity"] * 100}%
-          </Typography>
-          <Typography variant="body2" color="textPrimary" component="p">
-            Wind Speed : {this.currently["windSpeed"]} m/s
-          </Typography>
-          <Typography variant="body2" color="textPrimary" component="p">
-            Visibility : {this.currently["visibility"]} KM
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <IconButton
-            onClick={this.handleExpandClick}
-            aria-expanded={this.expanded}
-            aria-label="show more"
-          >
-            <ExpandMoreIcon />
-          </IconButton>
-        </CardActions>
-        <Collapse in={this.expanded} timeout="auto" unmountOnExit></Collapse>
-      </Card>
+      <div>
+        {this.props.weatherData[`${this.props.index}`] ? (
+          <Card>
+            <CardMedia
+              style={{ height: 0, paddingTop: "56.25%" }}
+              image={this.props.images[`${this.props.index}`]}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="headline" component="h2">
+                {this.props.index}
+              </Typography>
+              <Typography component="p" color="textSecondary">
+                {moment().format("dddd, MMMM Do YYYY h:mm a")}
+              </Typography>
+              <Typography component="p" variant="body2">
+                Current Temperature : {this.currently["temperature"]} C
+              </Typography>
+              <Typography component="p" variant="body2">
+                Maximum Temperature : {this.daily["data"][0]["temperatureHigh"]}{" "}
+                C
+              </Typography>
+              <Typography component="p" variant="body2">
+                Minimum Temperature : {this.daily["data"][0]["temperatureLow"]}{" "}
+                C
+              </Typography>
+              <Typography component="p" variant="body2">
+                Humidity : {this.currently["humidity"] * 100}%
+              </Typography>
+              <Typography component="p" variant="body2">
+                Wind Speed : {this.currently["windSpeed"]} m/s
+              </Typography>
+            </CardContent>
+          </Card>
+        ) : null}
+      </div>
     );
   }
 }
