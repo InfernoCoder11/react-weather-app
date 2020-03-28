3c89f95da69808758cb078b9e48a9a5f08bbc129
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2020-03-28 16:06:32 +0530

    Using local storage

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
 import React from "react";
 import CityAutocomplete from "./CityAutocomplete";
 import CityCard from "./CityCard";
+import NavBar from "./NavBar";
+import Grid from "@material-ui/core/Grid";
+import { Button } from "@material-ui/core";
 
 class App extends React.Component {
   state = {
     cities: [],
-    weatherData: {}
+    weatherData: {},
+    images: {}
   };
+  componentDidMount() {
+    const cities = localStorage.getItem("cities");
+    const weatherData = localStorage.getItem("weatherData");
+    const images = localStorage.getItem("images");
+    if (cities) this.setState({ order: JSON.parse(cities) });
+    if (weatherData) this.setState({ weatherData: JSON.parse(weatherData) });
+    if (images) this.setState({ images: JSON.parse(images) });
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem("cities", JSON.stringify(this.state.cities));
+    localStorage.setItem("weatherData", JSON.stringify(this.state.weatherData));
+    localStorage.setItem("images", JSON.stringify(this.state.images));
+  }
   addCity = city => {
     const cities = [...this.state.cities];
     cities.push(city);
@@ -17,23 +35,40 @@ class App extends React.Component {
     weatherData[city] = weather;
     this.setState({ weatherData });
   };
+  setCityImage = (city, link) => {
+    const images = { ...this.state.images };
+    images[city] = link;
+    this.setState({ images });
+  };
   render() {
     return (
       <div>
+        <NavBar />
         <CityAutocomplete
           cities={this.state.cities}
           addCity={this.addCity}
           setCityWeather={this.setCityWeather}
+          setCityImage={this.setCityImage}
         />
-        <span>
+        <Grid container spacing={24} style={{ padding: 24 }}>
           {Object.keys(this.state.weatherData).map(key => (
-            <CityCard
+            <Grid
+              item
+              xs={4}
+              sm={4}
+              lg={4}
+              xl={4}
               key={key}
-              index={key}
-              weatherData={this.state.weatherData}
-            />
+              style={{ padding: 5 }}
+            >
+              <CityCard
+                index={key}
+                weatherData={this.state.weatherData}
+                images={this.state.images}
+              />
+            </Grid>
           ))}
-        </span>
+        </Grid>
       </div>
     );
   }
