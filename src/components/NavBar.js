4f4c376b3030b5695dec2e28f931a966181e00a2
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2020-03-29 17:14:14 +0530

    Added Menu Icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,13 +2,26 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
 class NavBar extends React.Component {
+  handleClick = () => {
+    this.props.toggleDrawer();
+  };
   render() {
     return (
       <div>
         <AppBar position="static">
           <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={this.handleClick}
+            >
+              <MenuIcon />
+            </IconButton>
             <Typography variant="title" color="inherit">
               React Weather App
             </Typography>
